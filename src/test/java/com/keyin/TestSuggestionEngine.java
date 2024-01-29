a543c622bbbb55089cc550bdd6c667a73d4976a7
kostiantyn <kostiantyn@test.com>
2024-01-29 19:55:54 -0330

    tests completed

---
+++ src/test/java/com/keyin/TestSuggestionEngine.java
@@ -0,0 +1,45 @@
+package com.keyin;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TestSuggestionEngine {
+    private final Path path = Paths.get(ClassLoader.getSystemResource("words.txt").getPath());
+    private final SuggestionEngine suggestionEngineUnderTheTest = new SuggestionEngine();
+
+    @Test
+    public void testCorrectWordNoSuggestions() throws IOException {
+        suggestionEngineUnderTheTest.loadDictionaryData(this.path);
+        String correctWord = "word";
+
+        Assertions.assertEquals(suggestionEngineUnderTheTest.generateSuggestions(correctWord), "");
+    }
+
+    @Test
+    public void testErrorWordSuggestionsExist() throws IOException {
+        suggestionEngineUnderTheTest.loadDictionaryData(this.path);
+
+        String errorWord = "worf";
+        Assertions.assertFalse(suggestionEngineUnderTheTest.generateSuggestions(errorWord).isEmpty());
+    }
+
+    @Test
+    public void testErrorWordSuggestionsLimit() throws IOException {
+        suggestionEngineUnderTheTest.loadDictionaryData(this.path);
+
+        String errorWord = "worf";
+        Assertions.assertTrue(suggestionEngineUnderTheTest.generateSuggestions(errorWord).split("\\n").length <= 10);
+    }
+
+    @Test
+    public void testErrorWordSuggestionsContainCorrectWord() throws IOException {
+        suggestionEngineUnderTheTest.loadDictionaryData(this.path);
+
+        String errorWord = "worf";
+        Assertions.assertTrue(suggestionEngineUnderTheTest.generateSuggestions(errorWord).contains("word"));
+    }
+}
